89269b02421f5a5bf468fdb1391d7abe2011b1a0
wendel Anchieta <wendel.anchieta@valec.gov.br>
2018-12-19 14:18:10 -0200

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -25,8 +25,6 @@ export class CategoriasPage {
     this.categoriaService.findAll().subscribe(response => {
       this.items = response;  
     },
-    error => {
-      console.log(error);    
-    });
+    error => {});
   }
 }
