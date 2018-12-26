592a924dbb8984ea7718a25afbbe428a5e5b822d
wendel Anchieta <wendel.anchieta@valec.gov.br>
2018-12-26 15:01:45 -0200

    Carregando imagens dos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { API_CONFIG } from "../../config/api.config";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Rx";
 
 @Injectable()
 export class ProdutoService {
@@ -11,5 +12,10 @@ export class ProdutoService {
 
     findByCategoria(categoria_id : string) {
         return this.http.get(`${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}`);
+    }
+
+    getSmallImageFromBucket(id : string) : Observable<any> {
+        let url = `${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg`;
+        return this.http.get(url, {responseType: 'blob'});
     }
 }
